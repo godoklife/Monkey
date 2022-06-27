65b11979561ebf1d90f5afbea32bab094c59f296
Kim Yong Jun <godoklife1@gmail.com>
2022-06-27 15:52:41 +0900

    이니시얼 콤밋

---
+++ src/main/java/com/monkey/mpox/MpoxApplication.java
@@ -0,0 +1,13 @@
+package com.monkey.mpox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MpoxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MpoxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/monkey/mpox/MpoxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.monkey.mpox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MpoxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
