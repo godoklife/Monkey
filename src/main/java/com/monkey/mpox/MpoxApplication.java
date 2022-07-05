b65e078c12b410051c5a237ac79ee7e2990596bc
김지웅 <tgaggr@naver.com>
2022-07-05 17:32:43 +0900

    css 적용

--- src/main/java/com/monkey/mpox/MpoxApplication.java
+++ src/main/java/com/monkey/mpox/MpoxApplication.java
@@ -9,6 +9,6 @@ public class MpoxApplication {
     public static void main(String[] args){
 //        System.setProperty("file.encoding","UTF-8");    // 혹시 몰라서 심어둠. 배포한 뒤에도 이상없으면 @삭제 해도 됨.
         SpringApplication.run(MpoxApplication.class, args);
-    }
+    }//
 
 }
