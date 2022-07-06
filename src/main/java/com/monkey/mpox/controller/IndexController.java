e1b96b444db21f57c0707fb7e1159917ae855b70
Kim Yong Jun <godoklife1@gmail.com>
2022-07-06 16:13:42 +0900

    페이징처리 후 국가별 테이블 출력 고치는중( 1페이지 이외의 국가 출력 안됨)

--- src/main/java/com/monkey/mpox/controller/IndexController.java
+++ src/main/java/com/monkey/mpox/controller/IndexController.java
@@ -2,7 +2,7 @@ package com.monkey.mpox.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
+//
 @Controller
 public class IndexController {
 //
